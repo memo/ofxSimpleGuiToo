c37db7eeb6040b0e3ffb7d99de0439190c7f15ea
Memo Akten <memo@memo.tv>
2019-02-01 02:25:27 +0300

    addon_config and example qtcreator project

---
+++ addon_config.mk
@@ -0,0 +1,11 @@
+meta:
+	ADDON_NAME = ofxSimpleGuiToo
+	ADDON_DESCRIPTION = C++ openFrameworks addon for simple and very quick to setup GUI
+	ADDON_AUTHOR = Memo Akten, www.memo.tv
+	ADDON_TAGS = "GUI"
+	ADDON_URL = https://github.com/memo/ofxSimpleGuiToo
+
+common:
+	# dependencies with other addons, a list of them separated by spaces
+	# or use += in several lines
+	ADDON_DEPENDENCIES = ofxMSAInteractiveObject ofxXmlSettings
